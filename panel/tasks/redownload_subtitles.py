aec2306cb7ddfa0019a805947639c37d017d7a5d
tugcanolgun <zortoporto@gmail.com>
2021-03-19 21:18:04 +0100

    Fix folder deletion logic.

--- panel/tasks/redownload_subtitles.py
+++ panel/tasks/redownload_subtitles.py
@@ -13,7 +13,9 @@ logger = logging.getLogger(__name__)
 def remove_vtt_subtitles_folder(movie_content: MovieContent) -> None:
     full_path: str = movie_content.full_path
     main: str = movie_content.main_folder
-    folder_path: PosixPath = Path(full_path[: full_path.index(main) + len(main)])
+    folder_path: PosixPath = (
+        Path(full_path[: full_path.index(main) + len(main)]) / "vtt_subtitles"
+    )
     if folder_path.is_dir():
         try:
             shutil.rmtree(str(folder_path))
