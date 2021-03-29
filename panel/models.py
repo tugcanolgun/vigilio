9801b2c0b1014cffc565aa011ef22056709e993c
tugcanolgun <zortoporto@gmail.com>
2021-03-29 18:50:44 +0200

    User CoreModel as a base for models. Update foreign keys with strings to prevent circular import problem.

--- panel/models.py
+++ panel/models.py
@@ -3,7 +3,14 @@ from django.db import models
 from stream.models import MovieContent
 
 
-class MovieTorrent(models.Model):
+class CoreModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class MovieTorrent(CoreModel):
     source = models.TextField()
     name = models.CharField(max_length=255)
     movie_content = models.ForeignKey(
@@ -12,12 +19,11 @@ class MovieTorrent(models.Model):
     is_complete = models.BooleanField(default=False)
 
 
-class MudSource(models.Model):
+class MudSource(CoreModel):
     name = models.CharField(max_length=50)
     source = models.TextField()
     order = models.IntegerField(default=0)
     updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.name
